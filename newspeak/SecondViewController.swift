c9fef344ef60a8a93ebc70635817c9619d6e575e
kai ogita <kai-ogita@rich.co.jp>
2016-09-18 15:22:46 +0900

    Initial Commit

---
+++ newspeak/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  newspeak
+//
+//  Created by HW−0138 on 2016/09/18.
+//  Copyright © 2016年 appknow. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
