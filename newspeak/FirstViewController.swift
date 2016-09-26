3304e055f1f7bc69327cd96e0269713cbce74160
kai-ogita <hotsplusph@gmail.com>
2016-09-26 22:42:36 +0900

    指定の高さに

--- newspeak/FirstViewController.swift
+++ newspeak/FirstViewController.swift
@@ -12,7 +12,8 @@ import Alamofire
 
 class HomeTableViewController: UITableViewController {
     
-    var cellHeight:CGFloat = 0.0
+    var cellHeights:NSMutableArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +22,7 @@ class HomeTableViewController: UITableViewController {
         self.tableView.contentInset = UIEdgeInsetsMake(50, 0, (self.tabBarController?.tabBar.frame.height)!, 0)
         self.tableView.separatorColor = UIColor.clear
         self.edgesForExtendedLayout = UIRectEdge.all
-        tableView.estimatedRowHeight = 90
+        tableView.estimatedRowHeight = 100
         tableView.rowHeight = UITableViewAutomaticDimension
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -36,11 +37,14 @@ class HomeTableViewController: UITableViewController {
         cell.titleText.text = "GizMode"
         let imageURL = NSURL(string: "http://tech.admax.ninja/favicon.ico")
         cell.favicon.sd_setImage(with: imageURL as URL!)
-        let imageURL2 = NSURL(string: "http://www.wallpaper-box.com/smartphone/wp-content/uploads/2012/03/960x854x22eb464660b542b7b6384733.png")
+        let imageURL2 = NSURL(string: "http://o.aolcdn.com/hss/storage/midas/5d7db5caeef950c05866f7763194ada/204348455/main.jpg")
         cell.articleImage.sd_setImage(with: imageURL2 as URL!)
-        cell.descriptionLabel.text = "de \n omg orz \n f \n c"
-        //cell.descriptionLabel.sizeToFit()
+        cell.descriptionLabel.text = "de 25782385923547wurhfjsdnvxcmjshufr3eijdkqwnjfbgrt3444swc"
+        cell.descriptionLabel.sizeToFit()
+
         //self.cellHeight = cell.descriptionLabel.frame.height
+        self.cellHeights[indexPath.row] = cell.frame.height
+        print(self.cellHeights[indexPath.row])
         return cell
     }
     
@@ -50,18 +54,6 @@ class HomeTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
-    }
-    
-    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return super.tableView(tableView, heightForRowAt: indexPath)
-    }
-    
-    private func MAX(arg1:CGFloat,arg2:CGFloat) -> CGFloat{
-        if(Int(arg1) > Int(arg2)){
-            return arg1
-        }else{
-            return arg2
-        }
     }
 }
 
